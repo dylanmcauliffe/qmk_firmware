ab1b1a9d7235513ee431a3b440d953542fdfb1af
dylanmcauliffe <50005666+dylanmcauliffe@users.noreply.github.com>
2021-04-11 00:48:43 +0000

    Encoder fix

--- keyboards/keebio/bdn9/keymaps/dylanmcauliffe/config.h
+++ keyboards/keebio/bdn9/keymaps/dylanmcauliffe/config.h
@@ -3,3 +3,9 @@
 #ifdef RGB_MATRIX_ENABLE
 #    define RGB_MATRIX_KEYPRESSES
 #endif
+
+#undef ENCODERS_PAD_A
+#undef ENCODERS_PAD_B
+#define ENCODERS_PAD_A { A4, A15, A9 }
+#define ENCODERS_PAD_B { A8, B3, A10 }
+#define ENCODER_RESOLUTION 2
