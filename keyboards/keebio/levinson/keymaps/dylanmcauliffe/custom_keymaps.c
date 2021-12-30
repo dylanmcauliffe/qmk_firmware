5b203182ecbd5da83f56c1f6f37e4bde60a8524a
Dylan McAuliffe <50005666+dylanmcauliffe@users.noreply.github.com>
2021-12-30 12:36:54 -0800

    Keymap update

--- keyboards/keebio/levinson/keymaps/dylanmcauliffe/custom_keymaps.c
+++ keyboards/keebio/levinson/keymaps/dylanmcauliffe/custom_keymaps.c
@@ -1,35 +1,3 @@
-// Number Home Row Mods
-#define LG_1		LGUI_T(KC_1)
-#define LA_2		LALT_T(KC_2)
-#define LS_3		LSFT_T(KC_3)
-#define LC_4		LCTL_T(KC_4)
-
-#define RC_7		RCTL_T(KC_7)
-#define RS_8		RSFT_T(KC_8)
-#define RA_9		RALT_T(KC_9)
-#define RG_0		RGUI_T(KC_0)
-
-// Colemak Home Row Mods
-#define LG_A		LGUI_T(KC_A)
-#define LA_R		LALT_T(KC_R)
-#define LS_S		LSFT_T(KC_S)
-#define LC_T		LCTL_T(KC_T)
-
-#define RC_N		RCTL_T(KC_N)
-#define RS_E		RSFT_T(KC_E)
-#define RA_I		RALT_T(KC_I)
-#define RG_O		RGUI_T(KC_O)
-
-// QWERTY Home Row Mods
-#define LA_S		LALT_T(KC_S)
-#define LS_D		LSFT_T(KC_D)
-#define LC_F		LCTL_T(KC_F)
-
-#define RC_J		RCTL_T(KC_J)
-#define RS_K		RSFT_T(KC_K)
-#define RA_L		RALT_T(KC_L)
-#define RG_SCLN	RGUI_T(KC_SCLN)
-
 // Layer Triggers
 #define DF_CLMK	DF(_COLEMAKDHm)
 #define DF_QWTY	DF(_QWERTY)
