9ca52097135852383b0e8c7c52fb956d16b1c324
Dylan McAuliffe <50005666+dylanmcauliffe@users.noreply.github.com>
2022-11-25 16:30:19 -0800

    Custom Preonic config for

--- keyboards/absys/preonic/config.h
+++ keyboards/absys/preonic/config.h
@@ -1,20 +1,90 @@
-// Copyright 2022 Dylan McAuliffe (@dylanmcauliffe)
-// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #pragma once
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
+#include "config_common.h"
 
-/* disable debug print */
-//#define NO_DEBUG
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 6
 
-/* disable print */
-//#define NO_PRINT
+#define MATRIX_ROW_PINS { A10, A9, A8, B15, C13, C14, C15, A2, A3, A6 }
+#define MATRIX_COL_PINS { B11, B10, B2, B1, A7, B0 }
 
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+#define DIODE_DIRECTION COL2ROW
+
+#define DEBOUNCE 5
+
+#ifdef ENCODER_ENABLE
+	#define ENCODERS_PAD_A { B12 }
+	#define ENCODERS_PAD_B { B13 }
+
+	#define ENCODER_RESOLUTION 2
+	#define TAP_CODE_DELAY		5 // Needed for media keys
+#endif
+
+#ifdef AUDIO_ENABLE
+	#define AUDIO_PIN A5
+	#define AUDIO_PIN_ALT A4
+	#define AUDIO_PIN_ALT_AS_NEGATIVE
+
+	#define AUDIO_INIT_DELAY
+
+	#define STARTUP_SONG SONG(PLANCK_SOUND)
+	// #define STARTUP_SONG SONG(NO_SOUND)
+
+	#define DEFAULT_LAYER_SONGS \
+        { SONG(QWERTY_SOUND), SONG(COLEMAK_SOUND) }
+#endif
+
+#ifdef RGBLIGHT_ENABLE
+	#define RGB_DI_PIN			A1
+	
+	#define RGBLED_NUM			9
+	#define DRIVER_LED_TOTAL		RGBLED_NUM
+
+	#define RGBLIGHT_DEFAULT_MODE	RGBLIGHT_MODE_STATIC_LIGHT
+	#define RGBLIGHT_DEFAULT_HUE	16
+
+	#define RGBLIGHT_HUE_STEP	4
+	#define RGBLIGHT_SAT_STEP	4
+	#define RGBLIGHT_VAL_STEP	4
+//	#define RGBLIGHT_LIMIT_VAL	150
+
+	#define WS2812_PWM_DRIVER	PWMD2
+	#define WS2812_PWM_CHANNEL	2
+	#define WS2812_PWM_PAL_MODE	1
+	#define WS2812_DMA_STREAM	STM32_DMA1_STREAM2
+	#define WS2812_DMA_CHANNEL	2
+#endif
+
+#define TAPPING_TERM 150
+#define TAPPING_TERM_PER_KEY
+
+#define IGNORE_MOD_TAP_INTERRUPT
+
+//#define PERMISSIVE_HOLD
+#define PERMISSIVE_HOLD_PER_KEY
+
+//#define TAPPING_FORCE_HOLD
+#define TAPPING_FORCE_HOLD_PER_KEY
+
+//#define AUTO_SHIFT
+#define AUTO_SHIFT_REPEAT
+#define AUTO_SHIFT_TIMEOUT 150
+#define AUTO_SHIFT_TIMEOUT_PER_KEY
+#define NO_AUTO_SHIFT_TAB
--- keyboards/absys/preonic/rules.mk
+++ keyboards/absys/preonic/rules.mk
@@ -1,1 +1,18 @@
-# This file intentionally left blank
+MCU = STM32F303
+BOOTLOADER = stm32-dfu
+
+BOOTMAGIC_ENABLE = yes	# Enable Bootmagic Lite
+COMMAND_ENABLE = yes	# Commands for debug and configuration
+CONSOLE_ENABLE = yes	# Console for debug
+
+ENCODER_ENABLE = yes	# Encoder support	
+ENCODER_MAP_ENABLE = yes	# Encoder map support	
+EXTRAKEY_ENABLE = yes	# Audio control and System control
+MOUSEKEY_ENABLE = yes	# Mouse keys
+NKRO_ENABLE = yes		# Enable N-Key Rollover
+
+AUDIO_ENABLE = yes		# Audio output
+AUDIO_DRIVER = dac_basic
+
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+WS2812_DRIVER = pwm
